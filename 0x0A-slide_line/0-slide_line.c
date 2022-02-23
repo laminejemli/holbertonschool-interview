2366e80dfca452c8cf99d58da3abf31e8912a052
laminejemli <70071258+laminejemli@users.noreply.github.com>
2022-02-23 20:02:50 +0100

    Add files via upload

---
+++ 0x0A-slide_line/0-slide_line.c
@@ -0,0 +1,15 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "slide_line.h"
+
+/**
+ * slide_line -  slides and merges an array of integers
+ * 
+ * @line: array of integers
+ * @size: Number of elements in @array
+ * @direction: slided and mergerd to the direction
+ */
+int slide_line(int *line, size_t size, int direction)
+{
+    
+}
