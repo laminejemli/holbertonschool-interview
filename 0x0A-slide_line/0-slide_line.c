d86330484518b0c0f5c2205e20139947d634ddea
laminejemli <70071258+laminejemli@users.noreply.github.com>
2022-02-24 15:43:55 +0100

    Update 0-slide_line.c

--- 0x0A-slide_line/0-slide_line.c
+++ 0x0A-slide_line/0-slide_line.c
@@ -1,15 +1,130 @@
-#include <stdlib.h>
-#include <stdio.h>
-#include "slide_line.h"
+# inclure  " slide_line.h "
 
-/**
- * slide_line -  slides and merges an array of integers
- * 
- * @line: array of integers
- * @size: Number of elements in @array
- * @direction: slided and mergerd to the direction
+/* *
+* slide_line - glisser et fusionner des entiers dans un tableau donné s'ils sont identiques
+* les nombres et sont soit contigus soit séparés par des zéros
+* @line : tableau d'entiers donné
+* @size : nombre d'éléments dans le tableau
+* @direction : direction, définie par la macro SLIDE_LEFT ou SLIDE_RIGHT passée pour
+* glissement nécessaire
+*
+* Retour : 1 en cas de succès ou 0 en cas d'échec
  */
-int slide_line(int *line, size_t size, int direction)
+
+int  slide_line ( int *line, size_t size, int direction)
 {
-    
+	drapeau entier = 0 ;
+
+	si (taille < 1 || (direction != SLIDE_LEFT && direction != SLIDE_RIGHT))
+	{
+		retour (drapeau);
+	}
+	si (sens == SLIDE_LEFT)
+	{
+		flag = func_slide_left (ligne, taille);
+	}
+	si (sens == SLIDE_RIGHT)
+	{
+		flag = func_slide_right (ligne, taille);
+	}
+	retour (drapeau);
 }
+/* *
+* func_slide_left - lorsque la direction est à gauche, glisse et fusionne
+* @line : tableau d'ints donné à lire comme une ligne
+* @size : nombre d'éléments dans le tableau
+* Retour : 1 en cas de succès, 0 en cas d'échec
+*/
+	int  func_slide_left ( int *ligne, taille_t taille)
+	{
+		int je, j, drapeau = 0 ;
+		je = 0 ;
+		pour (j = 1 ; j < ( int )taille ;)
+		{
+			si (ligne[j] == 0 )
+			{
+				j++;
+				continuer ;
+			}
+			si (ligne[i] == ligne[j])
+			{
+				ligne[i] += ligne[j] ;
+				ligne[j] = 0 ;
+				drapeau = 1 ;
+				je++ ;
+				j++;
+			}
+			sinon  si (ligne[i] != ligne[j])
+			{
+				si (ligne[i] == 0 )
+				{
+					ligne[i] += ligne[j] ;
+					ligne[j] = 0 ;
+					drapeau = 1 ;
+					j++;
+				}
+				autre
+				{
+					je++ ;
+					si (je != j)
+					{
+						ligne[i] += ligne[j] ;
+						ligne[j] = 0 ;
+						drapeau = 1 ;
+					}
+					j++;
+				}
+			}
+		}
+		retour (drapeau);
+	}
+
+/* *
+* func_slide_right - direction donnée, glisse et fusionne les entiers dans le tableau
+* @line : tableau d'ints donné dans une ligne
+* @size : nombre d'éléments dans le tableau
+* Retour : 1 en cas de succès, 0 en cas d'échec
+*/
+	int  func_slide_right ( int *ligne, taille_t taille)
+	{
+		int je, j, drapeau = 0 ;
+		je = (( int )taille - 1 );
+		pour (j = (( int )taille - 2 ); j ​​>= 0 ;)
+		{
+			si (ligne[j] == 0 )
+			{
+				j-- ;
+				continuer ;
+			}
+			si (ligne[i] == ligne[j])
+			{
+				ligne[i] += ligne[j] ;
+				ligne[j] = 0 ;
+				drapeau = 1 ;
+				je--;
+				j-- ;
+			}
+			sinon  si (ligne[i] != ligne[j])
+			{
+				si (ligne[i] == 0 )
+				{
+					ligne[i] += ligne[j] ;
+					ligne[j] = 0 ;
+					drapeau = 1 ;
+					j-- ;
+				}
+				autre
+				{
+					je--;
+					si (je != j)
+					{
+						ligne[i] += ligne[j] ;
+						ligne[j] = 0 ;
+						drapeau = 1 ;
+					}
+					j-- ;
+				}
+			}
+		}
+		retour (drapeau);
+	}
