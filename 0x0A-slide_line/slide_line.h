0ceb3f0b0804cec1d0b746e7168b7e52278538e3
laminejemli <70071258+laminejemli@users.noreply.github.com>
2022-02-27 16:45:44 +0100

    Update slide_line.h

--- 0x0A-slide_line/slide_line.h
+++ 0x0A-slide_line/slide_line.h
@@ -1,15 +1,10 @@
-#ifndef SLIDE_LINE_H
-#define SLIDE_LINE_H
+#ifndef SLIDE_LINE
+#define SLIDE_LINE
 
 #include <stdio.h>
-#include <stdlib.h>
-#include <stddef.h>
-
-#define SLIDE_LEFT -2
-#define SLIDE_RIGHT 2
+#define SLIDE_LEFT 1
+#define SLIDE_RIGHT 0
 
 int slide_line(int *line, size_t size, int direction);
-int func_slide_left(int *line, size_t size);
-int func_slide_right(int *line, size_t size);
 
 #endif
