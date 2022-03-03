f466389fb514cb860cbb0145ad720b51ae7b1f21
laminejemli <70071258+laminejemli@users.noreply.github.com>
2022-03-03 14:04:20 +0100

    Update 0-stats.py

--- 0x06-log_parsing/0-stats.py
+++ 0x06-log_parsing/0-stats.py
@@ -1,45 +1,42 @@
 #!/usr/bin/python3
-
-""" script that reads stdin line by line and computes metrics """
+""" Script that reads stdin line by line and computes metrics."""
 
 import sys
 
+dlist = {"size": 0,
+         "lines": 1}
 
-def printstdin(date, size):
-    """ print information from stdin """
-    print("File size: {:d}".format(size))
-    for i in sorted(date.keys()):
-        if date[i] != 0:
-            print("{}: {:d}".format(i, date[i]))
+errors = {"200": 0, "301": 0, "400": 0, "401": 0,
+          "403": 0, "404": 0, "405": 0, "500": 0}
 
 
-statusCode = {"200": 0, "301": 0, "400": 0, "401": 0, "403": 0,
-              "404": 0, "405": 0, "500": 0}
-
-counter = 0
-size = 0
-
-try:
-    for line in sys.stdin:
-        if counter != 0 and counter % 10 == 0:
-            printstdin(statusCode, size)
-
-        listStatus = line.split()
-        counter += 1
-
-        try:
-            size += int(listStatus[-1])
-        except:
-            pass
-
-        try:
-            if listStatus[-2] in statusCode:
-                statusCode[listStatus[-2]] += 1
-        except:
-            pass
-    printstdin(statusCode, size)
+def printf():
+    """ Print codes and numbers"""
+    print("File size: {}".format(dlist["size"]))
+    for key in sorted(errors.keys()):
+        if errors[key] != 0:
+            print("{}: {}".format(key, errors[key]))
 
 
-except KeyboardInterrupt:
-    printstdin(statusCode, size)
-    raise
+def datasize(data):
+    """ Count file codes and size"""
+    dlist["size"] += int(data[-1])
+    if data[-2] in errors:
+        errors[data[-2]] += 1
+
+
+if __name__ == "__main__":
+    try:
+        for line in sys.stdin:
+            try:
+                datasize(line.split(" "))
+            except:
+                pass
+            if dlist["lines"] % 10 == 0:
+                printf()
+            dlist["lines"] += 1
+    except KeyboardInterrupt:
+        printf()
+        raise
+    printf()
+    
